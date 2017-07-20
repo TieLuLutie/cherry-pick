4fd67a1bdbbc6c98a3d648e9599bb0d5a3702194
zhongl <zhong.lunfu@gmail.com>
2017-07-20 17:35:49 +0800

    Support keyup w.

--- pick.js
+++ pick.js
@@ -1,9 +1,30 @@
 console.log("loaded");
-document.addEventListener("mouseover",function(event){
-  const e = event.target;
-  const pre = e.style.border
-  e.addEventListener("mouseout", function() {
-    e.style.border = pre;
-  });
-  e.style.border = "thick solid #0000FF";
-})
+
+var picked = {
+  parent: {},
+  unpick: function() {}
+};
+
+function pick(element) {
+  if ( "undefined" === typeof element.style ) return ;
+
+  const border = element.style.border;
+  element.style.border = "thick solid #0000FF";
+
+  picked.parent = element.parentElement;
+  picked.unpick = function() {
+      element.style.border = border;
+  }
+}
+
+document.addEventListener("mouseover",function(event) {
+  picked.unpick();
+  pick(event.target)
+});
+
+document.addEventListener("keyup", function(event) {
+  if (event.key == "w" ) {
+    picked.unpick();
+    pick(picked.parent);
+  }
+});
