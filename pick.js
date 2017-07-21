71aa05f73c20731769025879c2d33e9f0b00867a
zhongl <zhong.lunfu@gmail.com>
2017-07-21 14:56:11 +0800

    Clean code.

--- pick.js
+++ pick.js
@@ -1,30 +1,28 @@
-console.log("loaded");
+(function(picked) {
+  function pick(element) {
+    if ( "undefined" === typeof element.style ) return ;
 
-var picked = {
+    const border = element.style.border;
+    element.style.border = "thick solid #0000FF";
+
+    picked.parent = element.parentElement;
+    picked.unpick = function() {
+        element.style.border = border;
+    }
+  }
+
+  document.addEventListener("mouseover",function(event) {
+    picked.unpick();
+    pick(event.target)
+  });
+
+  document.addEventListener("keyup", function(event) {
+    if (event.key == "w" ) {
+      picked.unpick();
+      pick(picked.parent);
+    }
+  });
+}({
   parent: {},
   unpick: function() {}
-};
-
-function pick(element) {
-  if ( "undefined" === typeof element.style ) return ;
-
-  const border = element.style.border;
-  element.style.border = "thick solid #0000FF";
-
-  picked.parent = element.parentElement;
-  picked.unpick = function() {
-      element.style.border = border;
-  }
-}
-
-document.addEventListener("mouseover",function(event) {
-  picked.unpick();
-  pick(event.target)
-});
-
-document.addEventListener("keyup", function(event) {
-  if (event.key == "w" ) {
-    picked.unpick();
-    pick(picked.parent);
-  }
-});
+}));
